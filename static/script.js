771c80c7be84d14d7cd6762cbff8a73b555d4a1f
alecdavidson <alecdavidson@nau.edu>
2015-03-08 05:30:45 -0700

    tweaks

--- static/script.js
+++ static/script.js
@@ -1,72 +1,51 @@
-// App ID value from the dev portal. You can play
-// around with the supplied ID or replace it with
-// your own.
-var appid = "c5079ec6-3404-4e72-9061-78b7e3e87472";
-var endpointId;
-//get location
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(showPosition);
-	} else { 
-		$("#status").html("Geolocation is not supported by this browser.");
-	}
-}
-//show the lat and long
-function showPosition(position) {
-	$("#status").append("<br>Latitude: " + position.coords.latitude + 
-	"<br>Longitude: " + position.coords.longitude);  
-}
-// Create the client object using the App ID 
+// here's the App ID value from the portal:
+var appid = "DD90A374-0C06-456F-9D4F-E8038E6523D2";
+
+// create a client object using the App ID value from Step 2
 var client = respoke.createClient({
-	appId: appid,
-	developmentMode: true
+    appId: appid,
+    developmentMode: true
 });
 
-// "connect" event fired after successful connection to Respoke
-client.listen('connect', function() {
-	$("#status").html("Connected to Respoke as \"" + endpointId + "\"");
+// listen for the 'connect' event
+client.listen('connect', function () {
+    $("#status").html("Connected to Respoke!");
 });
 
-// Listen for incoming messages
+// now connect when the user clicks the 'Connect' button
+$("#doLogin").click(function () {
+    var endpoint = $("#endpoint").val();
+    client.connect({
+        endpointId: endpoint // your username is the endpoint
+    });
+});
+
+// listen for incoming messages
 client.listen('message', function(evt) {
-	$("#messages").append(
-		"<li>" + evt.message.message + "</li>"
-	);
+    $("#messages").append(
+        "<li>" + evt.message.message + "</li>"
+    );
 });
 
-// Connect to Respoke when the user clicks "connect"
-$("#doLogin").click(function() {
-	$("#status").html("Connecting...");
-	endpointId = $("#endpoint").val;
-	client.connect({
-		endpointId: endpointId // your username is the endpoint
-	});
-	showPosition(getLocation());
-});
+$("#sendMessage").click(function (){
 
-// Send message
-$("#sendMessage").click(function() {
-	// Get the recipients name
-	var remote = $("#remoteId").val();
+    // get the recipient name
+    var remote = $("#remoteId").val();
 
-	// Make an endpoint for that recipient
-	var endpoint = client.getEndpoint({
-		id: remote
-	});
+    // make an endpoint for that recipient
+    var endpoint = client.getEndpoint({ id: remote });
 
-	// Grab the text to send
-	var messageText = $("#textToSend").val();
+    // grab the text to send
+    var messageText = $("#textToSend").val();
 
-	// Send it
-	endpoint.sendMessage({
-		message: messageText
-	});
+    // send it
+    endpoint.sendMessage({ message : messageText });
 
-	// Show yourself the message
-	$("#messages").append(
-		"<li>" + messageText + "</li>"
-	);
+    // show yourself the message
+    $("#messages").append(
+        "<li>" + messageText + "</li>"
+    );
 
-	// Clear the text you just sent
-	$("#textToSend").val('');
+    // clear the text you just sent
+    $("#textToSend").val('');
 });
